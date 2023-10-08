42f5d6e61744e632b9db24c02408cb62e46ecfd0
Huáscar Suárez <oxhuascar321@outlook.com>
2023-10-07 20:44:09 -0400

    maps + buble added

--- lib/src/ui/screens/map_screen.dart
+++ lib/src/ui/screens/map_screen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_map/plugin_api.dart';
+import 'package:flutter/services.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
-import 'package:latlong2/latlong.dart';
+import 'package:h2o_app/src/helpers/printers.dart';
 
 class MapScreen extends StatefulWidget {
   const MapScreen({super.key});
@@ -13,16 +13,65 @@ class MapScreen extends StatefulWidget {
 class _MapScreenState extends State<MapScreen> {
   late GoogleMapController _controller;
 
+  Future<String> _loadMapStyle() async {
+    return await rootBundle.loadString('lib/src/models/custom_map_style.json');
+  }
+
+  final LatLng gulfOfMexicoCenter = const LatLng(25.1557, -94.8607);
+
+  final Set<LatLng> waterBodies = {
+    const LatLng(25.1557, -94.8607),
+  };
+
+  final LatLng upperLeftCorner = const LatLng(30.0, -90.0);
+  final LatLng lowerRightCorner = const LatLng(20.0, -80.0);
+  final double radius = 25;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text('Map Screen'),
+      appBar: AppBar(
+        title: const Text('Map Screen'),
+      ),
+      body: GoogleMap(
+        initialCameraPosition: const CameraPosition(
+          target: LatLng(24.9361108, -97.6591505),
+          zoom: 5,
         ),
-        body: GoogleMap(
-          initialCameraPosition: CameraPosition(
-            target: LatLng(0, 0),
-          ),
-        ));
+        onMapCreated: (controller) async {
+          final String style = await _loadMapStyle();
+
+          setState(() {
+            _controller = controller;
+            _controller.setMapStyle(style);
+          });
+        },
+        mapType: MapType.normal,
+        circles: waterBodies.map((LatLng waterBody) {
+          return Circle(
+            circleId: CircleId(waterBody.toString()),
+            center: waterBody,
+            radius: radius *
+                6378137, // Convert degrees to meters using Earth's radius.
+            fillColor: const Color.fromARGB(255, 56, 102, 129).withOpacity(0.3),
+            strokeColor: const Color.fromARGB(255, 56, 102, 129),
+            strokeWidth: 2, // Width of the circle outline
+          );
+        }).toSet(),
+        onTap: (LatLng latLng) {
+          for (var waterBody in waterBodies) {
+            if (LatLngBounds(
+              southwest: LatLng(
+                  waterBody.latitude - radius, waterBody.longitude - radius),
+              northeast: LatLng(
+                  waterBody.latitude + radius, waterBody.longitude + radius),
+            ).contains(latLng)) {
+              printWarning('Tapped on a water body');
+              break;
+            }
+          }
+        },
+      ),
+    );
   }
 }
